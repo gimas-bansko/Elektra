f6e1fadc9399f4ecc55d7a1f8fd31cd5046fe31f
gimas-bansko <63417802+gimas-bansko@users.noreply.github.com>
2024-12-25 21:58:06 +0200

    чиста инсталация на Django + локализация

---
+++ elektra/elektra/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for elektra project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elektra.settings')
+
+application = get_wsgi_application()
